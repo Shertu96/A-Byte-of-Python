0d2801572367a894ececf239d384a0e3b1d71812
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-11-24 20:49:51 +0300

    First test working with a book

---
+++ str_format.py
@@ -0,0 +1,16 @@
+age = 26
+name = 'Swaroop'
+print('Возраст {} -- {} лет.'.format(name, age))
+print('Почему {} забавляется с этим Python?'.format(name))
+
+i=5
+print(i)
+i=i+1
+print(i)
+s = '''Это многострочная строка. 
+Это вторая её строчка.'''
+print(s)
+
+s = 'This is a string. \
+The string is going on.'
+print(s)
